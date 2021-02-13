fd2eed0e69729865fb0c089db7a3ccacc457fc9c
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-13 19:21:21 +0400

    Transaction Management enabled

--- src/main/java/az/maqa/spring/aspect/SpringAopApplication.java
+++ src/main/java/az/maqa/spring/aspect/SpringAopApplication.java
@@ -2,8 +2,10 @@ package az.maqa.spring.aspect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringAopApplication {
 
 	public static void main(String[] args) {

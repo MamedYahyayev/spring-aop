31de75aba916b2ae7d985cc889beb3c04d467eb7
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-13 18:41:56 +0400

    initial commit

---
+++ src/test/java/az/maqa/spring/aspect/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package az.maqa.spring.aspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

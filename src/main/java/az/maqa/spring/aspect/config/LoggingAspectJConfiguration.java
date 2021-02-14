b7e6c95acdfee120ecac03dd6e195f862b6c9924
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-14 14:44:48 +0400

    enable AspectJAutoProxy

---
+++ src/main/java/az/maqa/spring/aspect/config/LoggingAspectJConfiguration.java
@@ -0,0 +1,10 @@
+package az.maqa.spring.aspect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectJConfiguration {
+
+}

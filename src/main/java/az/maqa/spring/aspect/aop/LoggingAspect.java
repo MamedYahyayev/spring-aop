2929efb60e09e178dc86e15aa05ff06f4a7340fa
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-14 20:52:18 +0400

    @Pointcut annotations added

--- src/main/java/az/maqa/spring/aspect/aop/LoggingAspect.java
+++ src/main/java/az/maqa/spring/aspect/aop/LoggingAspect.java
@@ -3,6 +3,7 @@ package az.maqa.spring.aspect.aop;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -31,5 +32,19 @@ public class LoggingAspect {
     public void logAfter() {
         log.info("This method executed after the createEmployee() method");
     }
+
+    /**
+     * @Pointcut annotation - prevent to copy paste same pointCut multiple times, instead of doing this we can create a new method and then add pointCut expression to that method.
+     * We are done. We can use this pointCut expression multiple times , just simply add method name.
+     */
+    @Pointcut("execution(* az.maqa.spring.aspect.controller.EmployeeController.*(*))")
+    public void forControllerPackage() {
+    }
+
+    @Before("forControllerPackage()")
+    public void logForControllerPackage(){
+        log.info("This method executes before all methods inside of controller package");
+    }
+
 
 }
